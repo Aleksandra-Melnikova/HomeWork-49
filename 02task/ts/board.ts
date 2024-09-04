9bc515d9a3910595bc825fcc54e8d65e84a6a8ad
Aleksandra Melnikova <Aleksa1992.14.11@mail.ru>
2024-09-04 22:38:26 +0600

    init board

---
+++ 02task/ts/board.ts
@@ -0,0 +1,26 @@
+    let row1:string = '';
+    let row2:string = '';
+    for (let i:number = 1; i <= 8; i++) {
+        if (i % 2 === 1) {
+            row1 += '#';
+        } else {
+            row1 += ' ';
+        }
+    }
+    for (let i:number = 1; i <= 8; i++) {
+        if (i % 2 === 1) {
+            row2 += ' ';
+        } else {
+            row2 += '#';
+        }
+    }
+    let picture:string = '';
+    for (let j:number = 1; j <= 8; j++) {
+        if (j % 2 === 1) {
+            picture += `${row1}` + `\n`;
+        } else {
+            picture += `${row2}` + `\n`;
+        }
+    }
+    console.log(picture);
+
