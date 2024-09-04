00e3650590c48e565a38f36e1dd221b144bf878c
Aleksandra Melnikova <Aleksa1992.14.11@mail.ru>
2024-09-04 22:41:53 +0600

    реализовала логику для четной строки

--- 02task/ts/board.ts
+++ 02task/ts/board.ts
@@ -7,13 +7,13 @@
             row1 += ' ';
         }
     }
-    // for (let i:number = 1; i <= 8; i++) {
-    //     if (i % 2 === 1) {
-    //         row2 += ' ';
-    //     } else {
-    //         row2 += '#';
-    //     }
-    // }
+     for (let i:number = 1; i <= 8; i++) {
+        if (i % 2 === 1) {
+            row2 += ' ';
+        } else {
+             row2 += '#';
+        }
+     }
     // let picture:string = '';
     // for (let j:number = 1; j <= 8; j++) {
     //     if (j % 2 === 1) {
