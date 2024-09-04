db127bdba5392a5008a05b77d2408909d9245289
Aleksandra Melnikova <Aleksa1992.14.11@mail.ru>
2024-09-04 22:40:45 +0600

    реализовала логику для нечетной строки

--- 02task/ts/board.ts
+++ 02task/ts/board.ts
@@ -7,20 +7,20 @@
             row1 += ' ';
         }
     }
-    for (let i:number = 1; i <= 8; i++) {
-        if (i % 2 === 1) {
-            row2 += ' ';
-        } else {
-            row2 += '#';
-        }
-    }
-    let picture:string = '';
-    for (let j:number = 1; j <= 8; j++) {
-        if (j % 2 === 1) {
-            picture += `${row1}` + `\n`;
-        } else {
-            picture += `${row2}` + `\n`;
-        }
-    }
-    console.log(picture);
+    // for (let i:number = 1; i <= 8; i++) {
+    //     if (i % 2 === 1) {
+    //         row2 += ' ';
+    //     } else {
+    //         row2 += '#';
+    //     }
+    // }
+    // let picture:string = '';
+    // for (let j:number = 1; j <= 8; j++) {
+    //     if (j % 2 === 1) {
+    //         picture += `${row1}` + `\n`;
+    //     } else {
+    //         picture += `${row2}` + `\n`;
+    //     }
+    // }
+    // console.log(picture);
 
